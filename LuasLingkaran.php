fb95102bc8b24ed95f20fa1ad0f410ca06b7fbd1
daffahidayat <daffaabalae10@gmail.com>
2025-11-13 16:36:55 +0700

    ada index nya

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -4,11 +4,22 @@ namespace App\Math;
 
 class LuasLingkaran {
     public const phi = 3.14;
-    public int $jari;
+    protected int $jari;
+
+    public function __construct($isijari = 1) {
+        $this->jari = $isijari;
+    }
+
+    public function tampil($nama = 'ban') {
+        $rumus = LuasLingkaran::phi * ($this->jari * $this->jari);
+        echo "Lingkaran {$nama} hasilnya adalah: {$rumus}";
+    }
+
+    public static function testing() {
+        echo " ini dari static";
+    }
+
+    public function __destruct() {
+        echo " udah ah cape";
+    }
 }
-
-$lingkaran = new LuasLingkaran();
-$lingkaran->jari = 5;
-
-$rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
-echo "Hasilnya adalah: ".$rumus;
