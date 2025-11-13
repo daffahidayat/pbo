c81af6cf35783a0875a8b28cb2212f0817b5b2de
daffahidayat <daffaabalae10@gmail.com>
2025-11-13 16:30:33 +0700

    kode terbaru

--- LuasLingkaran.php
+++ LuasLingkaran.php
@@ -7,7 +7,7 @@ class LuasLingkaran {
 }
 
 $lingkaran = new LuasLingkaran();
-$lingkaran->jari = 5;
+$lingkaran->jari = 12;
 
 $rumus = LuasLingkaran::phi * ($lingkaran->jari * $lingkaran->jari);
 echo "Hasilnya adalah: ".$rumus;
